90f39faff05d6e1b1753ca00a35f2d817e6744d8
sahalfreelance <110083545+sahalfreelance@users.noreply.github.com>
2024-11-11 20:41:49 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt install python3
+sudo apt install python3-pip
+pip3 install cloudscraper
+pip3 install json
+pip3 install uuid
+pip3 install loguru
+pip3 install fake_useragent
+clear
+python3 nodepay.py
